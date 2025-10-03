bf75ad4984d016f9fb91798bc9737143bdf32cce
Awais Ahmad <awais.ahmad@jeeny.me>
2025-10-03 12:53:02 +0500

    Add `user` module with basic Kotlin setup and Gradle configuration. Update project settings to include the new module.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,10 @@
 pluginManagement {
-	repositories {
-		maven { url = uri("https://repo.spring.io/snapshot") }
-		gradlePluginPortal()
-	}
+    repositories {
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
 }
 rootProject.name = "connectly"
 include("app")
+
+include("user")
---
+++ user/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jpa)
+}
+
+group = "com.communisol"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}

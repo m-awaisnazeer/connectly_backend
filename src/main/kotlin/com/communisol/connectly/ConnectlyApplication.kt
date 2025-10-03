a3b4bbce60d8ec70d0e835a1568d4b127ee42f07
Awais Ahmad <awais.ahmad@jeeny.me>
2025-10-03 11:58:06 +0500

    Initialize Spring Boot project with Kotlin backend. Add Gradle build configuration, basic application setup, and project structure.

---
+++ src/main/kotlin/com/communisol/connectly/ConnectlyApplication.kt
@@ -0,0 +1,11 @@
+package com.communisol.connectly
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConnectlyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConnectlyApplication>(*args)
+}
---
+++ src/test/kotlin/com/communisol/connectly/ConnectlyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.communisol.connectly
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConnectlyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
